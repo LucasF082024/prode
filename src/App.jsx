979e26723a854910276047a49aa5d686d837aee9
IgnacioOrellana12 <capacitaciones06.12@adviters.com>
2024-07-11 14:11:21 -0300

    some misterious changes have happpened

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { partidos } from "../mockUp/partido";
 import "./App.css";
 import ErrorPage from "./components/Error/Error";
 import Loading from "./components/Loading/Loading";
+import { Partidos } from "./components/Partidos/Partidos";
 import { useBuscarInfo } from "./hooks/useBuscarInfo";
 
 const App = () => {
