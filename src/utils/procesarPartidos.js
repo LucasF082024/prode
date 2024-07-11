72f9f27ab785a541e6ee2c40b017bb360f62a8cc
Milagros Guzman <capacitaciones06.03@adviters.com>
2024-07-11 16:06:17 -0300

    change: procesarPartidos

--- src/utils/procesarPartidos.js
+++ src/utils/procesarPartidos.js
@@ -1,19 +1,19 @@
 import { COINCIDENCIA_EXACTA,ADIVINAR_GANADOR,ADIVINAR_LADO } from "../constants/puntos"
 
-const coincidenciaExacta = (partido, inpLocal,inpVisitor) =>{
-    const [local,visitor]  = partido.score
+const coincidenciaExacta = (score, inpLocal,inpVisitor) =>{
+    const [local,visitor]  = score
     return (local.goals === inpLocal && visitor.goals === inpVisitor)  ? COINCIDENCIA_EXACTA : 0;
 }
-const coincidenciaLado = (partido, inpLocal,inpVisitor) =>{
-  const [local,visitor]  = partido.score
+const coincidenciaLado = (score, inpLocal,inpVisitor) =>{
+  const [local,visitor]  = score
   let total =0;
   local.goals === inpLocal ? 
     visitor.goals === inpVisitor ? total+=ADIVINAR_LADO*2  : total+=ADIVINAR_LADO 
     : visitor.goals === inpVisitor ? total+=ADIVINAR_LADO  : 0
   return total
 }
-const adivinaGanador = (partido, inpLocal,inpVisitor) =>{
-  const [local,visitor]  = partido.score
+const adivinaGanador = (score, inpLocal,inpVisitor) =>{
+  const [local,visitor]  = score
   let ganador = {}
 
   if(local.goals > visitor.goals){
@@ -26,10 +26,21 @@ const adivinaGanador = (partido, inpLocal,inpVisitor) =>{
 }
 
 export const calcularPuntaje = (partido, inpLocal, inpVisitor)=>{
+  const {score, penalties} = partido;
   let puntaje=0;
-  puntaje += coincidenciaLado(partido, inpLocal, inpVisitor);
-  puntaje += adivinaGanador(partido, inpLocal, inpVisitor);
-  puntaje += coincidenciaExacta(partido, inpLocal, inpVisitor);
+  if (typeof inpLocal !== "number" || typeof inpVisitor !== "number") throw new Error ("Surgio un error con los valores ingresados")
+  
+  if (partido.penalties) {
+    puntaje += coincidenciaLado(penalties, inpLocal, inpVisitor);
+    puntaje += adivinaGanador(penalties, inpLocal, inpVisitor);
+    puntaje += coincidenciaExacta(penalties, inpLocal, inpVisitor);
+    return puntaje;
+  }
+  
+  
+  puntaje += coincidenciaLado(score, inpLocal, inpVisitor);
+  puntaje += adivinaGanador(score, inpLocal, inpVisitor);
+  puntaje += coincidenciaExacta(score, inpLocal, inpVisitor);
 
   return puntaje;
 }
