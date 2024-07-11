c3189b156fd9d0c3d1a705753c47648dffec6393
IgnacioOrellana12 <capacitaciones06.12@adviters.com>
2024-07-11 13:02:51 -0300

    App pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import { partidos } from "../mockUp/partido";
 import "./App.css";
 import ErrorPage from "./components/Error/Error";
 import Loading from "./components/Loading/Loading";
+import { Partidos } from "./components/Partidos/Partidos";
 import { useBuscarInfo } from "./hooks/useBuscarInfo";
 import Alumnos from "./pages/Alumnos";
 
 const App = () => {
-  const { data, loading, error } = useBuscarInfo();
+  // const { data, loading, error } = useBuscarInfo();
 
-  if (loading) {
-    return <Loading />;
-  }
+  // if (loading) {
+  //   return <Loading />;
+  // }
 
-  if (error) {
-    return <ErrorPage error={error} />;
-  }
-  return <Alumnos data={data} />;
+  // if (error) {
+  //   return <ErrorPage error={error} />;
+  // }
+  // return <Alumnos data={data} />;
+  // console.log(partidos.data)
+  return (
+    <Partidos partidos={partidos.data}/>
+  )
 };
 
 export default App;
