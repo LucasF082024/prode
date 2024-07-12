1584bbceee68bf9c3fcc68d7bca3c0f265914f1f
Milagros Guzman <capacitaciones06.03@adviters.com>
2024-07-12 16:01:51 -0300

    add: home page, logo and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
-
-
+import styles from "./home.module.css";
+import {PAGE_TITLE} from "../../constants/header.constants"
+import { useNavigate } from "react-router-dom";
+import { RUTAS } from "../../constants/routers.constants";
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <>Home</>
+        <section className={styles.home}>
+            <div className={styles.cover}>
+                <p>{`¡Predecí los resultados de la copa con ${PAGE_TITLE}!`}</p>
+                <div className={styles.buttons}>
+                    <button onClick={()=>{navigate(`/${RUTAS.LOGIN}`)}}>Iniciar Sesión</button>
+                    <button  onClick={()=>{navigate(`/${RUTAS.REGISTER}`)}}>Registrarse</button>
+                </div>
+            </div>
+           
+        </section>
     )
 }
 export default Home;
