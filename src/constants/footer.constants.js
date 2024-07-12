0cc7579b0f87b0b420da50c744b53a79227d36fd
Milagros Guzman <capacitaciones06.03@adviters.com>
2024-07-12 15:32:12 -0300

    add: footer constants

---
+++ src/constants/footer.constants.js
@@ -0,0 +1,18 @@
+export const MEMBERS =[
+    {
+        NAME: "Benjamín",
+        SURNAME:"Alegre"
+    },
+    {
+        NAME: "Lucas",
+        SURNAME:"Ferreira"
+    },
+    {
+        NAME: "Ignacio",
+        SURNAME:"Oreyano"
+    },
+    {
+        NAME: "Milagros",
+        SURNAME:"Guzmán"
+    }
+] 
